19ef976c3f65e10369329c3f274fad5730f00c35
laupwing <loc_strict@hotmail.com>
2023-07-06 10:49:31 +0200

    twilio

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Twilio\Rest\Client;
 
 class SmsController extends Controller
 {
@@ -61,6 +62,15 @@ class SmsController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    private function sendMessage($message, $recipients)
+    {
+        $account_sid = getenv("TWILIO_SID");
+        $auth_token = getenv("TWILIO_AUTH_TOKEN");
+        $twilio_number = getenv("TWILIO_NUMBER");
+        $client = new Client($account_sid, $auth_token);
+        $client->messages->create($recipients, ['from' => $twilio_number, 'body' => $message]);
     }
 
     public function send(Request $request) {
